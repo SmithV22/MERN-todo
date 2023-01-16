ede254dd0507df41aa60585ab19ccbb0a026c468
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-01-16 07:38:47 -0500

    first commit

---
+++ server/models/todo.model.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose') ;
+const TodoSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Please enter your todo name'],
+            minLength: [2, 'Name must be longer than 2 characters'],
+        },
+        description: {
+            type: String,
+            minLength: [2, 'Name must be longer than 2 characters'],
+        },
+        dueBy: {
+            type: Date,
+            
+        },
+        important: {
+            type: String,
+            required: [true, 'Please say if this is important'],
+            enum: {
+                values: ['Yes', 'No']
+            },
+        },
+    },
+    {
+        timestamps: true,
+    }
+) ;
+
+module.exports = mongoose.model('Todo', TodoSchema) ;
